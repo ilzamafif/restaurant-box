908317d53e503bc5ef63fc1ef16bf3bd9826b323
ilzamafif <ilzamafif27@gmail.com>
2021-04-30 20:29:17 +0700

    scss

--- webpack.common.js
+++ webpack.common.js
@@ -1,38 +1,39 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/scripts/index.js'),
+  entry: {
+    main: "src/scripts/index.js",
+    scss: "src/styles/main.scss",
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-        ],
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(["css-loader", "sass-loader", "style-loader"]),
       },
     ],
   },
   plugins: [
+    new ExtractTextPlugin({
+      filename: "css/app.bundle.css",
+      allChunks: true,
+    }),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/templates/index.html'),
-      filename: 'index.html',
+      template: path.resolve(__dirname, "src/templates/index.html"),
+      filename: "index.html",
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
+          from: path.resolve(__dirname, "src/public/"),
+          to: path.resolve(__dirname, "dist/"),
         },
       ],
     }),
