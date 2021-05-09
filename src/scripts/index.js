38226f07c225077a431aeab2818301021102d46a
ilzamafif <ilzamafif27@gmail.com>
2021-05-09 13:03:16 +0700

    membuat app shell dan konfig drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,43 +1,10 @@
-import "regenerator-runtime"; /* for async await transpile */
+import "regenerator-runtime";
 import "../styles/main.css";
 import "../styles/responsive.css";
-import result from "../public/DATA.json";
-console.log(result);
-restaurantList(result);
+import App from "./views/app";
 
-function restaurantList(result) {
-  let restaurant = "";
-  let menu = result.restaurants;
-  menu.forEach((res, i) => {
-    restaurant += /*html*/ `
-    <div class="card" tabindex="0">
-      <img tabindex="0" src="${res.pictureId}" width="100%" alt="${res.name}" />
-      <div class="card-content">
-        <div class="card-content-head">
-          <h3 class="title" tabindex="0">${res.name}</h3>
-          <div class="rating" tabindex="0">
-            <span class="iconify star" data-icon="bi:star-fill" data-inline="false"></span>
-            <p>${res.rating}</p>
-          </div>
-        </div>
-        <h3 class="city" tabindex="0">Loc : ${res.city}</h3>
-        <p tabindex="0">
-          ${res.description}
-        </p>
-      </div>
-    </div>
-    `;
-  });
-
-  document.querySelector("#posts").innerHTML = restaurant;
-
-  const newSilde = () => {
-    const bars = document.querySelector(".bars-toggler");
-    const nav = document.querySelector(".nav-list");
-
-    bars.addEventListener("click", () => {
-      nav.classList.toggle("nav-active");
-    });
-  };
-  newSilde();
-}
+const app = new App({
+  button: document.querySelector(".bars-toggler"),
+  drawer: document.querySelector(".nav-list"),
+  content: document.querySelector("#content"),
+});
