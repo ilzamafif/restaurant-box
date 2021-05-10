1c2d56521e7d8f9d3cd059cad9bd0388420b9c23
ilzamafif <ilzamafif27@gmail.com>
2021-05-10 11:31:05 +0700

    notification

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -7,6 +7,7 @@ const CONFIG = {
   DATABASE_NAME: "restaurant-box-database",
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: "restaurant",
+  WEB_SOCKET_SERVER: "wss://stormy-badlands-06326.herokuapp.com",
 };
 
 export default CONFIG;
